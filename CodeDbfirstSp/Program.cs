f37af1dfa3d3cfaeec2f8571810a4f94fa266ee2
yogesh2019 <yogesh.khichi@gmail.com>
2023-01-25 02:49:37 +0530

    update object

--- CodeDbfirstSp/Program.cs
+++ CodeDbfirstSp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,16 +13,11 @@ namespace CodeDbfirstSp
         {
             var context = new PlutoDbContext();
 
-            var course = new Cours
-            {
-                Title = "New course",
-                Description = "new description",
-                Price = 19,
-                Level = 1,
-                Author = new Author { AuthorID = 1, Name = "new author" }
-            };
-            context.Courses.Add(course);
+            var course = context.Courses.Find(16);
+
+            course.Title = "modified course";
             context.SaveChanges();
+
         }
     }
 }
