ad0112ae469bd5798748f3d5c59d22aa196f2119
yogesh2019 <yogesh.khichi@gmail.com>
2023-01-24 15:54:39 +0530

    Add project files.

---
+++ CodeDbfirstSp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeDbfirstSp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbContext = new PlutoDbContext();
+            var courses = dbContext.GetCourses();
+            foreach(var c in courses)
+            {
+                Console.WriteLine(c.Title);
+            }
+            Console.ReadLine();
+        }
+    }
+}
