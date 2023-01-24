d0020a24fee1957dfd21a2df7c94c6dcf477cc91
yogesh2019 <yogesh.khichi@gmail.com>
2023-01-24 17:14:02 +0530

    linq in action

--- CodeDbfirstSp/Program.cs
+++ CodeDbfirstSp/Program.cs
@@ -9,16 +9,23 @@ namespace CodeDbfirstSp
     class Program
     {
         static void Main(string[] args)
-        {
+        { 
             var context = new PlutoDbContext();
             var query =
                 from c in context.Courses
                 where c.Title.Contains("C#")
                 orderby c.Title
                 select c;
+                
             foreach (var course in query)
                 Console.WriteLine(course.Title);
-         
+
+
+
+            var courseExt = context.Courses.Where(c => c.Title.Contains("c#")).ToList();
+            /*foreach( var c in courseExt)
+                Console.WriteLine(c.Title);*//*foreach( var c in courseExt)
+                Console.WriteLine(c.Title);*/
             Console.ReadLine();
         }
     }
