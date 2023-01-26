ef39c5021e013a33139384d767bde8af651e3a86
yogesh2019 <yogesh.khichi@gmail.com>
2023-01-26 23:10:46 +0530

    IUnit of Work Interface

---
+++ CodeDbfirstSp/Cores/Repositories/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeDbfirstSp.Cores.Repositories
+{
+    interface IUnitOfWork : IDisposable
+    {
+        ICourseRepository Course { get; }
+        int Complete();
+    }
+}
